8a7425da8379f1b6357ab6c5fcde75591e921b4e
matthias zimmermann <zimmermann@xbrain.ch>
2023-08-24 16:17:25 +0200

    add intheritance experiment

---
+++ contracts/experiment/IB.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+interface IB {
+
+    function getAfromB() external view returns(uint256);
+    function getB() external view returns(uint256);
+    function setB(uint256 newA) external;
+}
---
+++ contracts/experiment/IC.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+interface IC {
+
+    function getAfromC() external view returns(uint256);
+    function getBfromC() external view returns(uint256);
+
+    function getC() external view returns(uint256);
+    function setC(uint256 newA) external;
+}
