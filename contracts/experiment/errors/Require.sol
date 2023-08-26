5e69afdaa271049558c616a4e6537a49fcd55e0c
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-08-26 20:10:46 +0000

    add TestExperimentRequireRevert

---
+++ contracts/experiment/errors/Require.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+contract Require {
+
+    error AsmallerThanB_S();
+    error AsmallerThanB_M(uint a);
+    error AsmallerThanB_L(uint a, uint b);
+
+    uint256 private _b;
+
+    constructor() {
+        _b = 42;
+    }
+
+    function isAlargerThanBRequire_S(uint a) external view returns(bool isLarger) {
+        require(a > _b, "ERROR:ABC-001");
+
+        return true;
+    }
+
+    function isAlargerThanBRequire_M(uint a) external view returns(bool isLarger) {
+        require(a > _b, "ERROR:ABC-001:A_IS_SMALLER");
+
+        return true;
+    }
+
+    function isAlargerThanBRequire_L(uint a) external view returns(bool isLarger) {
+        require(a > _b, "ERROR:ABC-001:A_IS_SMALLER_THAN_B");
+
+        return true;
+    }
+}
---
+++ contracts/experiment/errors/Revert.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+contract Revert {
+
+    error AsmallerThanB_S();
+    error AsmallerThanB_M(uint a);
+    error AsmallerThanB_L(uint a, uint b);
+
+    uint256 private _b;
+
+    constructor() {
+        _b = 42;
+    }
+
+    function isAlargerThanBRevert_S(uint a) external view returns(bool isLarger) {
+        if(a <= _b) {
+            revert AsmallerThanB_S();
+        }
+
+        return true;
+    }
+
+    function isAlargerThanBRevert_M(uint a) external view returns(bool isLarger) {
+        if(a <= _b) {
+            revert AsmallerThanB_M(a);
+        }
+
+        return true;
+    }
+
+    function isAlargerThanBRevert_L(uint a) external view returns(bool isLarger) {
+        if(a <= _b) {
+            revert AsmallerThanB_L(a, _b);
+        }
+
+        return true;
+    }
+}
