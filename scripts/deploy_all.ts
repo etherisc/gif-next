b9385e155f93b10d8a04fa6313c61f997db67099
Marc Doerflinger <marc@doerf.li>
2023-09-15 10:54:10 +0000

    deployment of nftidlib (#30)

---
+++ scripts/deploy_all.ts
@@ -0,0 +1,75 @@
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { deployContract, verifyContract } from "./deploy_helper";
+import { logger } from "./logger";
+
+
+async function main() {
+//   const signer = process.env.WALLET_MNEMONIC ? ethers.Wallet.fromPhrase(process.env.WALLET_MNEMONIC as string).connect(ethers.provider) : undefined;
+//   console.log("signer: " + signer?.address);
+
+    const { instanceOwner, productOwner, poolOwner } = await getNamedAccounts();
+    
+    const { address: nfIdLibAddress } = await deployContract("NftIdLib", instanceOwner);
+
+
+
+
+//   // deploy registry
+//   const registry = await ethers.deployContract("Registry", [], {
+//     signer,
+//     libraries: {
+//       NftIdLib: nftIdLibAdr,
+//     },
+//   });
+//   await registry.waitForDeployment();
+//   const registryAdr = registry.target;
+//   console.log(
+//     `Registry deployed to ${registryAdr}`
+//   );
+
+//   // wait for 5 confirmations
+//   console.log("waiting for 5 confirmations");
+//   await registry.deploymentTransaction()?.wait(5);
+
+//   await verifyContract(registryAdr, []);
+
+//   // deploy ChainNft
+//   const chainNftFactory = await ethers.getContractFactory("ChainNft", signer);
+//   const chainNftDeployed = await chainNftFactory.deploy(registryAdr);
+//   const chainNftAdr = chainNftDeployed.target;
+//   console.log(
+//     `ChainNft deployed to ${chainNftAdr}`
+//   );
+
+//   // wait for 5 confirmations
+//   console.log("waiting for 5 confirmations");
+//   await chainNftDeployed.deploymentTransaction()?.wait(5);
+
+//   await verifyContract(chainNftAdr, [registryAdr]);
+
+//   const registryContract = await ethers.getContractAt("Registry", registryAdr, signer);
+//   await registryContract.initialize(chainNftAdr);
+//   console.log(`Registry initialized with ChainNft @ ${chainNftAdr}`);
+}
+
+async function getNamedAccounts(): Promise<{ instanceOwner: Signer; productOwner: Signer; poolOwner: Signer; }> {
+    const signers = await ethers.getSigners();
+    const instanceOwner = signers[0];
+    const productOwner = signers[1];
+    const poolOwner = signers[2];
+    logger.info(`instanceOwner: ${instanceOwner.address},\n productOwner: ${productOwner.address},\n poolOwner: ${poolOwner.address}`);
+    return { instanceOwner, productOwner, poolOwner }; 
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    logger.error(error);
+    process.exitCode = 1;
+});
+
+
+
+
--- scripts/deploy_helper.ts
+++ scripts/deploy_helper.ts
@@ -1,20 +1,45 @@
-import hre from "hardhat";
-import { AddressLike } from "ethers";
+import hre, { ethers } from "hardhat";
+import { AddressLike, Signer, ContractTransactionResponse } from "ethers";
+import { logger } from "./logger";
 
 export async function verifyContract(address: AddressLike, constructorArgs: any[]) {
-    console.log("verifying contract @ address: " + address);
-    // console.log("args: " + args);
+    logger.debug("verifying contract @ address: " + address);
     try {
         await hre.run("verify:verify", {
             address: address,
             constructorArguments: constructorArgs,
         });
-        console.log("Contract verified\n\n");
+        logger.info("Contract verified\n\n");
     } catch (err: any) {
         if (err.message.toLowerCase().includes("already verified")) {
-            console.log("Contract is already verified! \n\n");
+            logger.info("Contract is already verified! \n\n");
         } else {
             throw err;
         }
     }
 };
+
+export async function deployContract(contractName: string, instanceOwner: Signer): Promise<{
+    address: AddressLike; 
+    deploymentTransaction: ContractTransactionResponse | null;
+}> {
+    const contractFactory = await ethers.getContractFactory(contractName, instanceOwner);
+    const deployTxResponse = await contractFactory.deploy();
+    const deployedContractAddress = deployTxResponse.target;
+    logger.info(
+        `${contractName} deployed to ${deployedContractAddress}`
+    );
+
+    if (process.env.SKIP_VERIFICATION?.toLowerCase() !== "true") {
+        // wait for 5 confirmations
+        logger.debug("waiting for 5 confirmations");
+        // TODO: make this configurable
+        await deployTxResponse.deploymentTransaction()?.wait(5);
+        
+        await verifyContract(deployedContractAddress, []);
+    } else {
+        logger.debug("Skipping verification");
+    }
+
+    return { address: deployedContractAddress, deploymentTransaction: deployTxResponse.deploymentTransaction() };
+}
