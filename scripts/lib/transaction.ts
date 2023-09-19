14ce46ba9f58776e848a6e29cbe530e97db9e7d9
Marc Doerflinger <marc@doerf.li>
2023-09-19 08:48:50 +0000

    clean log (#30)

--- scripts/lib/transaction.ts
+++ scripts/lib/transaction.ts
@@ -11,12 +11,12 @@ export function getFieldFromLogs(tx: ContractTransactionReceipt, abiInterface: I
     
     logs?.forEach(log => {
         const parsedLog = abiInterface.parseLog({ data: log.data, topics: log.topics as string[] });
-        logger.debug(`parsedLog.name: ${parsedLog?.name}`);
+        // logger.debug(`parsedLog.name: ${parsedLog?.name}`);
         if (parsedLog?.name === eventName) {
             // destructuring assignment to fetch the value of the field `fieldName` from the object `p.args`
             const { [fieldName]: v } = parsedLog.args;
             value = v;
-            logger.debug(`${eventName}: ${value}`);
+            // logger.debug(`${eventName}: ${value}`);
         }
     });
 
