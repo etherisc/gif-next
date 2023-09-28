9de1dec8191bb2429a91f6da08c7d1600c3ac9c9
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-09-28 16:26:22 +0000

    remove instance product module

--- contracts/instance/module/product/ProductModule.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.19;
-
-import {IProductService} from "../../service/IProductService.sol";
-import {IProductModule} from "./IProductModule.sol";
-
-abstract contract ProductModule is IProductModule {
-}
