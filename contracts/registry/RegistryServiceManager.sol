9a53e6f1aa87509c0c1049ba90deb685074e7176
rapiddenis <41779817+rapidddenis@users.noreply.github.com>
2024-01-02 08:24:06 -0500

    deploy AccessManager

--- contracts/registry/RegistryServiceManager.sol
+++ contracts/registry/RegistryServiceManager.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {AccessManager} from "@openzeppelin5/contracts/access/manager/AccessManager.sol";
+
+import {ContractDeployerLib} from "../shared/ContractDeployerLib.sol";
+
 import {Registry} from "./Registry.sol";
 import {IVersionable} from "../shared/IVersionable.sol";
 import {ProxyManager} from "../shared/ProxyManager.sol";
@@ -10,13 +14,30 @@ import {RegistryService} from "./RegistryService.sol";
 contract RegistryServiceManager is
     ProxyManager
 {
-    RegistryService private _registryService;
+    bytes32 constant public ACCESS_MANAGER_CREATION_CODE_HASH = 0x0;
+
+    RegistryService private _registryService; 
+
+    AccessManager _accessManager;
 
     /// @dev initializes proxy manager with registry service implementation and deploys registry
     constructor(
     )
         ProxyManager()
     {
+        bytes memory encodedConstructorArguments = abi.encode(msg.sender);
+
+        bytes memory accessManagerCreationCode = ContractDeployerLib.getCreationCode(
+            type(AccessManager).creationCode,
+            encodedConstructorArguments);
+
+        address accessManagerAddress = ContractDeployerLib.deploy(
+            accessManagerCreationCode,
+            ACCESS_MANAGER_CREATION_CODE_HASH);
+
+        _accessManager = AccessManager(accessManagerAddress);
+
+
         IVersionable versionable = deploy(
             address(new RegistryService()), 
             type(Registry).creationCode);
@@ -39,5 +60,13 @@ contract RegistryServiceManager is
         returns (RegistryService registryService)
     {
         return _registryService;
+    }
+
+    function getAccessManager()
+        external
+        view
+        returns (RegistryService registryService)
+    {
+        return _accessManager;
     }
 }
