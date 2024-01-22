70b61ae0b0484b9228dc7ad2de28053b7bdf756e
Marc Doerflinger <marc@doerf.li>
2024-01-22 14:20:21 +0000

    fix pool (#146)

--- contracts/components/Pool.sol
+++ contracts/components/Pool.sol
@@ -168,21 +168,19 @@ contract Pool is BaseComponent, IPoolComponent {
         _poolService.setBundleFee(bundleNftId, fee);
     }
 
+    function getInitialSetupInfo() public view returns (ISetup.PoolSetupInfo memory setupInfo) {
+        return ISetup.PoolSetupInfo(
+            _productNftId,
+            TokenHandler(address(_token)),
+            _collateralizationLevel,
+            _initialPoolFee,
+            _initialStakingFee,
+            _initialPerformanceFee,
+            _isVerifying,
+            _wallet
+        );
+    }
     function getSetupInfo() public view returns (ISetup.PoolSetupInfo memory setupInfo) {
-        // TODO fix this
-        if (getNftId().eq(zeroNftId())) {
-            return ISetup.PoolSetupInfo(
-                _productNftId,
-                TokenHandler(address(0)),
-                _collateralizationLevel,
-                _initialPoolFee,
-                _initialStakingFee,
-                _initialPerformanceFee,
-                _isVerifying,
-                _wallet
-            );
-        } 
-
         InstanceReader reader = _instance.getInstanceReader();
         return reader.getPoolSetupInfo(getNftId());
     }
@@ -205,16 +203,7 @@ contract Pool is BaseComponent, IPoolComponent {
         return (
             info,
             abi.encode(
-                ISetup.PoolSetupInfo(
-                    getProductNftId(),
-                    TokenHandler(address(0)),
-                    _collateralizationLevel,
-                    _initialPoolFee,
-                    _initialStakingFee,
-                    _initialPerformanceFee,
-                    _isVerifying,
-                    _wallet
-                )
+                getInitialSetupInfo()
             )
         );
     }
