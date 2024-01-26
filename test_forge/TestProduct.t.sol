a4ec6fb75f2eb2d05b74857afb21aff3538d36a3
Marc Doerflinger <marc@doerf.li>
2024-01-26 15:55:22 +0000

    set product nft id into components

--- test_forge/TestProduct.t.sol
+++ test_forge/TestProduct.t.sol
@@ -340,17 +340,18 @@ contract TestProduct is TestGifBase {
         );
 
         productNftId = productService.register(address(product));
-        // TODO: this could probably be done within register and also set on pool and distribution on one shot
         product.setProductNftId(productNftId);
         vm.stopPrank();
 
         vm.startPrank(distributionOwner);
+        distribution.setProductNftId(productNftId);
         Fee memory distributionFee = FeeLib.toFee(UFixedLib.zero(), 10);
         distribution.setFees(distributionFee);
         vm.stopPrank();
 
 
         vm.startPrank(poolOwner);
+        pool.setProductNftId(productNftId);
         Fee memory poolFee = FeeLib.toFee(UFixedLib.zero(), 10);
         pool.setFees(poolFee, FeeLib.zeroFee(), FeeLib.zeroFee());
         vm.stopPrank();
