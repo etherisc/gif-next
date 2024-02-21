7145fb8f1aff181c7843d09e1c804278703db89f
Matthias Zimmermann <zimmermann@xbrain.ch>
2024-02-21 15:41:25 +0000

    adapt registry.ts

--- contracts/registry/RegistryDeployer.sol
+++ contracts/registry/RegistryDeployer.sol
@@ -22,9 +22,8 @@ contract RegistryDeployer
     
     event LogConsole(string message);
 
-    // TODO 1) check if this does deploy outside forge test
-    // TODO 2) provide registry service manager as parameter
-    // TODO 3) configure registry service manager and complete deployment
+    // TODO 1) provide registry service manager as parameter
+    // TODO 2) configure registry service manager and complete deployment
     constructor(address registryOwner) {
         emit LogConsole("debug 1.1");
 
