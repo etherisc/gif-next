449e9b27b2b65c1bbb467a2c6cb4fe6ca6c3bf26
Marc Doerflinger <marc@doerf.li>
2024-03-04 14:03:10 +0000

    validate targets (#203)

--- contracts/instance/module/IAccess.sol
+++ contracts/instance/module/IAccess.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {ShortString, ShortStrings} from "@openzeppelin/contracts/utils/ShortStrings.sol";
 
 import {RoleId} from "../../types/RoleId.sol";
@@ -34,15 +33,15 @@ interface IAccess {
     error ErrorIAccessRoleNameNotUnique(RoleId roleId, ShortString name);
     error ErrorIAccessRoleInvalidUpdate(RoleId roleId, bool isCustom);
     error ErrorIAccessRoleIsCustomIsImmutable(RoleId roleId, bool isCustom, bool isCustomExisting);
-    error ErrorIAccessSetLockedForNonexstentRole(RoleId roleId);
     error ErrorIAccessGrantNonexstentRole(RoleId roleId);
     error ErrorIAccessRevokeNonexstentRole(RoleId roleId); 
     error ErrorIAccessRenounceNonexstentRole(RoleId roleId);
 
     error ErrorIAccessTargetAddressZero();
-    error ErrorIAccessTargetAlreadyExists(address target, ShortString name);
+    error ErrorIAccessTargetDoesNotExist(ShortString name);
+    error ErrorIAccessTargetAddressDoesNotExist(address target);
+    error ErrorIAccessTargetExists(address target, ShortString name);
     error ErrorIAccessTargetNameEmpty(address target);
     error ErrorIAccessTargetNameExists(address target, address existingTarget, ShortString name);
-    error ErrorIAccessSetLockedForNonexstentTarget(address target);
     error ErrorIAccessTargetLocked(address target);
 }
