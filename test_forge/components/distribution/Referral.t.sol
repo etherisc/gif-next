3380e9fcfb19bff9a16c72761173e9646ada0065
Marc Doerflinger <marc@doerf.li>
2024-03-19 10:10:28 +0000

    add another test (#240)

--- test_forge/components/distribution/Referral.t.sol
+++ test_forge/components/distribution/Referral.t.sol
@@ -11,7 +11,7 @@ import {ISetup} from "../../../contracts/instance/module/ISetup.sol";
 import {NftId, NftIdLib} from "../../../contracts/types/NftId.sol";
 import {POLICY} from "../../../contracts/types/ObjectType.sol";
 import {POOL_OWNER_ROLE, PRODUCT_OWNER_ROLE} from "../../../contracts/types/RoleId.sol";
-import {ReferralLib} from "../../../contracts/types/Referral.sol";
+import {ReferralId, ReferralLib} from "../../../contracts/types/Referral.sol";
 import {ReferralTestBase} from "./ReferralTestBase.sol";
 import {RiskId, RiskIdLib} from "../../../contracts/types/RiskId.sol";
 import {Seconds, SecondsLib} from "../../../contracts/types/Seconds.sol";
@@ -60,7 +60,7 @@ contract ReferralTest is ReferralTestBase {
         assertFalse(distributionService.referralIsValid(distributionNftId, ReferralLib.toReferralId(distributionNftId, "UNKNOWN")), "referral is valid");
     }
 
-    function test_Referral_applyAndUnderwriteWithReferral() public {
+    function test_Referral_underwriteWithReferral() public {
         _setupTestData(true);
         _setupPoolAndProduct();
 
@@ -137,6 +137,144 @@ contract ReferralTest is ReferralTestBase {
 
         ISetup.DistributionSetupInfo memory distributionSetupInfo = instanceReader.getDistributionSetupInfo(distributionNftId);
         assertEq(distributionSetupInfo.sumDistributionOwnerFees, 11, "sumDistributionOwnerFees not 11");
+    }
+
+    function test_Referral_underwriteMultipleWithReferral() public {
+        _setupTestData(true);
+        _setupPoolAndProduct();
+
+        // GIVEN - two policies to underwrite
+        vm.startPrank(registryOwner);
+        token.transfer(customer, 1000);
+        token.transfer(customer2, 1000);
+        vm.stopPrank();
+
+        vm.startPrank(productOwner);
+
+        RiskId riskId = RiskIdLib.toRiskId("42x4711");
+        bytes memory data = "bla di blubb";
+        SimpleProduct dproduct = SimpleProduct(address(product));
+        dproduct.createRisk(riskId, data);
+
+        vm.stopPrank();
+
+        vm.startPrank(customer);
+
+        ISetup.ProductSetupInfo memory productSetupInfo = instanceReader.getProductSetupInfo(productNftId);
+        token.approve(address(productSetupInfo.tokenHandler), 1000);
+        // revert("checkApprove");
+
+        SimpleDistribution sdistribution = SimpleDistribution(address(distribution));
+        referralId = sdistribution.createReferral(
+            distributorNftId,
+            referralCode,
+            UFixedLib.toUFixed(5, -2),
+            maxReferrals,
+            expiryAt,
+            referralData);
+
+        NftId policyNftId = dproduct.createApplication(
+            customer,
+            riskId,
+            1000,
+            SecondsLib.toSeconds(30),
+            "",
+            bundleNftId,
+            referralId
+        );
+        assertTrue(policyNftId.gtz(), "policyNftId was zero");
+        assertEq(chainNft.ownerOf(policyNftId.toInt()), customer, "customer not owner of policyNftId");
+
+        NftId policyNftId2 = dproduct.createApplication(
+            customer,
+            riskId,
+            1000,
+            SecondsLib.toSeconds(30),
+            "",
+            bundleNftId,
+            referralId
+        );
+        assertTrue(policyNftId.gtz(), "policyNftId was zero");
+        assertEq(chainNft.ownerOf(policyNftId.toInt()), customer, "customer not owner of policyNftId");
+        
+        vm.stopPrank();
+
+        // WHEN
+        vm.startPrank(productOwner);
+        dproduct.underwrite(policyNftId, true, TimestampLib.blockTimestamp()); 
+        dproduct.underwrite(policyNftId2, true, TimestampLib.blockTimestamp()); 
+
+        // THEN - check 13 tokens in distribution wallet (120 premium ), 887 tokens in customer wallet, 10100 tokens in pool wallet
+        assertTrue(instanceReader.getPolicyState(policyNftId) == ACTIVE(), "policy state not ACTIVE");
+        assertTrue(instanceReader.getPolicyState(policyNftId2) == ACTIVE(), "policy state not ACTIVE");
+
+        IBundle.BundleInfo memory bundleInfo = instanceReader.getBundleInfo(bundleNftId);
+        assertEq(bundleInfo.lockedAmount, 2000, "lockedAmount not 1000");
+        assertEq(bundleInfo.balanceAmount, 10000 + 200, "balanceAmount not 1100");
+        
+        assertEq(token.balanceOf(distribution.getWallet()), 28, "distribution balance not 14");
+        
+        assertEq(instanceBundleManager.activePolicies(bundleNftId), 2, "expected one active policy");
+        
+        IDistribution.DistributorInfo memory distributorInfo = instanceReader.getDistributorInfo(distributorNftId);
+        assertEq(distributorInfo.numPoliciesSold, 2, "numPoliciesSold not 2");
+        assertEq(distributorInfo.sumCommisions, 6, "sumCommisions not 6");
+
+        ISetup.DistributionSetupInfo memory distributionSetupInfo = instanceReader.getDistributionSetupInfo(distributionNftId);
+        assertEq(distributionSetupInfo.sumDistributionOwnerFees, 22, "sumDistributionOwnerFees not 22");
+        vm.stopPrank();
+
+        // --------------------------------------------------------------
+        // GIVEN one more policy to underwrite with a different referral
+        vm.startPrank(distributionOwner);
+        NftId distributorNftId2 = distribution.createDistributor(
+            customer2,
+            distributorType,
+            distributorData);
+        vm.stopPrank();
+
+        vm.startPrank(customer2);
+        token.approve(address(productSetupInfo.tokenHandler), 1000);
+
+        ReferralId referralId2 = sdistribution.createReferral(
+            distributorNftId2,
+            "SAVE2!!!",
+            UFixedLib.toUFixed(5, -2),
+            maxReferrals,
+            expiryAt,
+            referralData);
+
+        NftId policyNftId3 = dproduct.createApplication(
+            customer2,
+            riskId,
+            1000,
+            SecondsLib.toSeconds(30),
+            "",
+            bundleNftId,
+            referralId2
+        );
+
+        // WHEN 
+        dproduct.underwrite(policyNftId3, true, TimestampLib.blockTimestamp()); 
+
+        // THEN 
+        assertTrue(instanceReader.getPolicyState(policyNftId3) == ACTIVE(), "policy3 state not ACTIVE");
+
+        bundleInfo = instanceReader.getBundleInfo(bundleNftId);
+        assertEq(bundleInfo.lockedAmount, 3000, "lockedAmount not 1000");
+        assertEq(bundleInfo.balanceAmount, 10000 + 300, "balanceAmount not 1100");
+        
+        assertEq(token.balanceOf(distribution.getWallet()), 42, "distribution balance not 14");
+        
+        assertEq(instanceBundleManager.activePolicies(bundleNftId), 3, "expected one active policy");
+        
+        IDistribution.DistributorInfo memory distributorInfo2 = instanceReader.getDistributorInfo(distributorNftId2);
+        assertEq(distributorInfo2.numPoliciesSold, 1, "numPoliciesSold not 2");
+        assertEq(distributorInfo2.sumCommisions, 3, "sumCommisions not 6");
+
+        distributionSetupInfo = instanceReader.getDistributionSetupInfo(distributionNftId);
+        assertEq(distributionSetupInfo.sumDistributionOwnerFees, 33, "sumDistributionOwnerFees not 33");
+        vm.stopPrank();
     }
 
     function _setupPoolAndProduct() internal {
