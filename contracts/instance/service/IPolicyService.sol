115f86c7c3b12b9c72ceabc9f15f0909ebba3472
Matthias Zimmermann <zimmermann@xbrain.ch>
2024-04-09 09:04:10 +0000

    fix rebase bug

--- contracts/instance/service/IPolicyService.sol
+++ contracts/instance/service/IPolicyService.sol
@@ -18,6 +18,9 @@ import {Fee} from "../../types/Fee.sol";
 
 interface IPolicyService is IService {
 
+    error ErrorPolicyServiceProductMismatch(NftId applicationNftId, NftId expectedProductNftId, NftId actualProductNftId);
+    error ErrorPolicyServicePolicyStateNotApplied(NftId applicationNftId);
+
     error ErrorIPolicyServiceInsufficientAllowance(address customer, address tokenHandlerAddress, uint256 amount);
     error ErrorIPolicyServicePremiumAlreadyPaid(NftId policyNftId, uint256 premiumPaidAmount);
     error ErrorIPolicyServicePolicyNotActivated(NftId policyNftId);
