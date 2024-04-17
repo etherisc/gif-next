52acda327fc40da5f4f93c2e60ee5098d2fd0a55
rapiddenis <41779817+rapidddenis@users.noreply.github.com>
2024-04-17 16:07:11 +0000

    remove requires

--- contracts/registry/RegistryServiceManager.sol
+++ contracts/registry/RegistryServiceManager.sol
@@ -14,12 +14,14 @@ import {TokenRegistry} from "./TokenRegistry.sol";
 contract RegistryServiceManager is
     ProxyManager
 {
+    error ErrorRegistryAccessManagerAuthorityZero();
+    error ErrorRegistryAccessManagerRegistryZero();
+
     bytes32 constant public ACCESS_MANAGER_CREATION_CODE_HASH = 0x0;
 
     RegistryService private immutable _registryService;
 
     /// @dev initializes proxy manager with registry service implementation and deploys registry
-    //registryServiceManager = new RegistryServiceManager{salt: config.CONFIG_SALT()}(releaseAccessManager, registryAddress, config.CONFIG_SALT());
     constructor(
         address authority, // used by implementation 
         address registry, // used by implementation 
@@ -27,11 +29,15 @@ contract RegistryServiceManager is
     ) 
         ProxyManager(registry)
     {
-        require(authority > address(0), "RegistryServiceManager: initial authority is 0");
-        require(registry > address(0), "RegistryServiceManager: registry is 0");
+        if(authority == address(0)) {
+            revert ErrorRegistryAccessManagerAuthorityZero();
+        }
+
+        if(registry == address(0)) {
+            revert ErrorRegistryAccessManagerRegistryZero();
+        }
         
         // implementation's initializer func `data` argument
-        //RegistryService srv = new RegistryService{salt: salt}();
         RegistryService srv = new RegistryService{ salt: salt }();
         bytes memory data = abi.encode(registry, address(this), authority);
         IVersionable versionable = deployDetermenistic(
