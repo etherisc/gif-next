64e8a666f0b0f6693447e5b1601de275b37d46ca
Marc Doerflinger <marc@doerf.li>
2024-07-02 15:16:23 +0000

    add TODO

--- contracts/instance/module/IBundle.sol
+++ contracts/instance/module/IBundle.sol
@@ -13,6 +13,7 @@ interface IBundle {
         NftId poolNftId;
         Fee fee; // bundle fee on net premium amounts
         bytes filter; // required conditions for applications to be considered for collateralization by this bundle
+        // TODO: is lifetime **AND** expiredAt necessary?
         Seconds lifetime; // lifetime of bundle after creation
         Timestamp expiredAt; // no new policies starting with this timestamp
         Timestamp closedAt; // no open policies, locked amount = 0
