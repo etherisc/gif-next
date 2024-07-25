d49269474778dd45d96ed6b4128914f15765fd1b
Marc Doerflinger <marc@doerf.li>
2024-07-25 13:06:56 +0000

    fix tests

--- test/component/product/ProductPayout.t.sol
+++ test/component/product/ProductPayout.t.sol
@@ -216,9 +216,9 @@ contract TestProductClaim is GifTest {
         // THEN
         // checking last of 11 logs
         assertEq(entries.length, 11, "unexpected number of logs");
-        assertEq(entries[8].topics[0], keccak256("LogClaimServicePayoutProcessed(uint96,uint24,uint96,address,uint96,uint96)"), "unexpected log signature");
+        assertEq(entries[8].topics[0], keccak256("LogClaimServicePayoutProcessed(uint96,uint40,uint96,address,uint96,uint96)"), "unexpected log signature");
         assertEq(entries[8].emitter, address(claimService), "unexpected emitter");
-        (uint96 nftIdInt ,uint24 payoutIdInt, uint96 payoutAmntInt) = abi.decode(entries[8].data, (uint96,uint24,uint96));
+        (uint96 nftIdInt, uint40 payoutIdInt, uint96 payoutAmntInt) = abi.decode(entries[8].data, (uint96,uint40,uint96));
         assertEq(nftIdInt, policyNftId.toInt(), "unexpected policy nft id");
         assertEq(payoutIdInt, payoutId.toInt(), "unexpected payout id");
         assertEq(payoutAmntInt, payoutAmountInt, "unexpected payout amount");
