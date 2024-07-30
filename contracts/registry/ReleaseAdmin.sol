642e97fb0b90f7bc65012ca8cc164b47335d07ec
rapiddenis <41779817+rapidddenis@users.noreply.github.com>
2024-07-30 21:39:13 +0000

    ReleaseAdmin: account for role/target existance, add release already locked check, dissalow locking of release admin, add comments

--- contracts/registry/ReleaseAdmin.sol
+++ contracts/registry/ReleaseAdmin.sol
@@ -1,8 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
 import {AccessAdmin} from "../authorization/AccessAdmin.sol";
 import {IAccess} from "../authorization/IAccess.sol";
+import {IAccessAdmin} from "../authorization/IAccessAdmin.sol";
 import {IService} from "../shared/IService.sol";
 import {IServiceAuthorization} from "../authorization/IServiceAuthorization.sol";
 import {ObjectType, ObjectTypeLib, ALL, RELEASE} from "../type/ObjectType.sol";
@@ -16,9 +17,14 @@ contract ReleaseAdmin is
     AccessAdmin
 {
     error ErrorReleaseAdminCallerNotReleaseRegistry(address caller);
+    error ErrorReleaseAdminNotService(address notService);
+    error ErrorReleaseAdminReleaseAlreadyLocked();
 
     /// @dev release core roles
     string public constant RELEASE_REGISTRY_ROLE_NAME = "ReleaseRegistryRole";
+
+    /// @dev release core targets
+    string public constant RELEASE_ADMIN_TARGET_NAME = "ReleaseAdmin";
 
 
     modifier onlyReleaseRegistry() {
@@ -53,6 +59,7 @@ contract ReleaseAdmin is
     {
         // set and initialize access manager for this release admin
         // admin role is granted before it is created
+        // _deployer initialized here
         _initializeAuthority(accessManager);
 
         // create basic release independent setup
@@ -64,7 +71,7 @@ contract ReleaseAdmin is
 
     /// @dev Sets up authorizaion for specified service.
     /// For all authorized services its authorized functions are enabled.
-    /// Permissioned function: Access is restricted to release manager.
+    /// Permissioned function: Access is restricted to release registry.
     function authorizeService(
         IServiceAuthorization serviceAuthorization,
         IService service,
@@ -78,6 +85,10 @@ contract ReleaseAdmin is
         _authorizeServiceFunctions(serviceAuthorization, service, serviceDomain, releaseVersion);
     }
 
+    /// @dev Locks/unlocks all release targets.
+    /// For all authorized services of release its authorized functions are disabled/enabled.
+    /// Permissioned function: Access is restricted to release registry.
+    /// Note: onlyReleaseRegistry() modifier is used to prevent dead lock.
     function setReleaseLocked(bool locked)
         external
         onlyReleaseRegistry()
@@ -85,10 +96,20 @@ contract ReleaseAdmin is
         _setReleaseLocked(locked);
     }
 
+    /// @dev Lock/unlock specific service of release.
+    /// Permissioned function: Access is restricted to release registry.
+    /// Note: service locked with this function wont'be unlocked by setReleaseLocked(false).
     function setServiceLocked(IService service, bool locked)
         external
         restricted()
     {
+        // assume all targets except release admin are services
+        // ensure release admin can not be locked
+        if(address(service) == address(this)) {
+            revert ErrorReleaseAdminNotService(address(service));
+        }
+
+        // will check for release lock
         _setTargetClosed(address(service), locked);
     }
 
@@ -133,16 +154,18 @@ contract ReleaseAdmin is
             serviceDomain, 
             releaseVersion);
 
-        _createRole(
-            serviceRoleId, 
-            toRole({
-                adminRoleId: ADMIN_ROLE(),
-                roleType: RoleType.Contract,
-                maxMemberCount: 1,
-                name: ObjectTypeLib.toVersionedName(
-                    baseName, 
-                    "ServiceRole", 
-                    versionInt)}));
+        if(!roleExists(serviceRoleId)) {
+            _createRole(
+                serviceRoleId, 
+                toRole({
+                    adminRoleId: ADMIN_ROLE(),
+                    roleType: RoleType.Contract,
+                    maxMemberCount: 1,
+                    name: ObjectTypeLib.toVersionedName(
+                        baseName, 
+                        "ServiceRole", 
+                        versionInt)}));
+        }
 
         _grantRoleToAccount( 
             serviceRoleId,
@@ -175,6 +198,20 @@ contract ReleaseAdmin is
                 releaseVersion);
             }
 
+            if(!roleExists(authorizedRoleId)) {
+                // create role for authorized domain
+                _createRole(
+                    authorizedRoleId, 
+                    toRole({
+                        adminRoleId: ADMIN_ROLE(),
+                        roleType: RoleType.Contract,
+                        maxMemberCount: 1,
+                        name: ObjectTypeLib.toVersionedName(
+                            ObjectTypeLib.toName(authorizedDomain), 
+                            "Role", 
+                            releaseVersion.toInt())}));
+            }
+
             // get authorized functions for authorized domain
             IAccess.FunctionInfo[] memory authorizatedFunctions = serviceAuthorization.getAuthorizedFunctions(
                 serviceDomain, 
@@ -190,7 +227,12 @@ contract ReleaseAdmin is
     function _setReleaseLocked(bool locked)
         private
     {
-        ReleaseAccessManagerCloneable(authority()).setReleaseLocked(locked);
+        ReleaseAccessManagerCloneable accessManager = ReleaseAccessManagerCloneable(authority());
+        if(accessManager.isReleaseLocked() == locked) {
+            revert ErrorReleaseAdminReleaseAlreadyLocked();
+        }
+
+        accessManager.setReleaseLocked(locked);
     }
 
     //--- private initialization functions -------------------------------------------//
@@ -199,6 +241,8 @@ contract ReleaseAdmin is
         private 
         onlyInitializing()
     {
+        _createTarget(address(this), RELEASE_ADMIN_TARGET_NAME, false, false);
+
         // TODO why release registry role is calculated?
         //RoleId releaseRegistryRoleId = RoleIdLib.roleForType(RELEASE());
         RoleId releaseRegistryRoleId = RELEASE_REGISTRY_ROLE();
