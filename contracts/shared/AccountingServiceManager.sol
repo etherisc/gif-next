77f65bd203c60e85d0771ad18d03b1ead161016c
Marc Doerflinger <marc@doerf.li>
2024-08-15 11:55:27 +0000

    copy&paste implementation of AccountingService  (#599)

---
+++ contracts/shared/AccountingServiceManager.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import {AccountingService} from "./AccountingService.sol";
+import {IVersionable} from "../upgradeability/IVersionable.sol";
+import {ProxyManager} from "../upgradeability/ProxyManager.sol";
+
+contract AccountingServiceManager is ProxyManager {
+
+    AccountingService private _accountingService;
+
+    /// @dev initializes proxy manager with service implementation 
+    constructor(
+        address authority, 
+        address registry,
+        bytes32 salt
+    )
+    {
+        AccountingService svc = new AccountingService();
+        bytes memory data = abi.encode(registry, authority);
+        IVersionable versionable = initialize(
+            registry,
+            address(svc), 
+            data,
+            salt);
+
+        _accountingService = AccountingService(address(versionable));
+    }
+
+    //--- view functions ----------------------------------------------------//
+    function getAccountingService()
+        external
+        view
+        returns (AccountingService)
+    {
+        return _accountingService;
+    }
+}
