9294f0957517a2fa5d4d7a42f50e65ea65a03137
Matthias Zimmermann <zimmermann@xbrain.ch>
2024-09-03 18:13:37 +0000

    amend hh deploy script

--- scripts/libs/instance.ts
+++ scripts/libs/instance.ts
@@ -151,6 +151,7 @@ export async function deployAndRegisterMasterInstance(
         [],
         { 
             libraries: {
+                AccessAdminLib: libraries.accessAdminLibAddress,
                 AmountLib: libraries.amountLibAddress,
                 ClaimIdLib: libraries.claimIdLibAddress,
                 DistributorTypeLib: libraries.distributorTypeLibAddress,
