f46275587adfc956a37bdbd18fd40b898312ebb0
Marc Doerflinger <marc@doerf.li>
2024-09-17 13:17:46 +0000

    remove KeyValueStore (#660)

--- test/mock/MockSizeKeyValueStore.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.20;
-
-import {KeyValueStore} from "../../contracts/shared/KeyValueStore.sol";
-
-contract MockSizeKeyValueStore is KeyValueStore {
-        function _setupLifecycle() internal override {}
-}
